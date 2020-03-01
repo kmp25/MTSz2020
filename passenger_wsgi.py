6293f87cd6c0917894591a737eafd32edd82a19a
kmp25 <krzysztof@parol.net.pl>
2020-03-01 11:27:08 +0100

    remove passenger.py

--- passenger_wsgi.py
+++
@@ -1,5 +0,0 @@
-import sys, os
-sys.path.append(os.getcwd())
-os.environ['DJANGO_SETTINGS_MODULE'] = "MTSz2020.settings" 
-from django.core.wsgi import get_wsgi_application
-application = get_wsgi_application()
